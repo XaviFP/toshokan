ceb7b253773e077f06912a90e48793c822e92ab3
Xavi Ferrer <xavi@delape.net>
2023-02-20 19:40:37 +0100

    fix use of config password. use config

--- dealer/src/config.rs
+++ dealer/src/config.rs
@@ -1,29 +1,32 @@
 use std::{env, fmt};
 
 pub struct DBConfig {
-	pub user:     String,
-	pub password: String,
-	pub name:     String,
-	pub host:     String,
-	pub port:     String,
+    pub user: String,
+    pub password: String,
+    pub name: String,
+    pub host: String,
+    pub port: String,
 }
 
 impl fmt::Display for DBConfig {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-
-        write!(f, "connect_timeout=2 host={} user={} pasword={} dbname={}", self.host, self.user, self.password, self.name)
+        write!(
+            f,
+            "connect_timeout=5 host={} user={} password={} dbname={}",
+            self.host, self.user, self.password, self.name
+        )
     }
 }
 
 pub struct GRPCServerConfig {
-	pub host:              String,
-	pub port:              String,
+    pub host: String,
+    pub port: String,
 }
 
 impl fmt::Display for GRPCServerConfig {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         //TODO Investigate how to use hostname instead
-        write!(f, "0.0.0.0:{}",self.port) // self.host, 
+        write!(f, "0.0.0.0:{}", self.port) // self.host,
     }
 }
 
@@ -34,7 +37,13 @@ const DB_USER: &str = "DB_USER";
 const DB_PASSWORD: &str = "DB_PASSWORD";
 
 pub fn load_db_config() -> Result<DBConfig, String> {
-    let mut config = DBConfig{ user: "".to_owned(), password: "".to_owned(), name: "".to_owned(), host: "".to_owned(), port: "".to_owned() };
+    let mut config = DBConfig {
+        user: "".to_owned(),
+        password: "".to_owned(),
+        name: "".to_owned(),
+        host: "".to_owned(),
+        port: "".to_owned(),
+    };
 
     let host = env::var(DB_HOST);
     if host.is_err() {
@@ -69,7 +78,7 @@ pub fn load_db_config() -> Result<DBConfig, String> {
         return Err(format!("missing environment variable: {}", DB_PASSWORD));
     }
 
-    config.host = password.unwrap();
+    config.password = password.unwrap();
 
     Ok(config)
 }
@@ -78,18 +87,27 @@ const GRPC_SERVER_HOST: &str = "GRPC_SERVER_HOST";
 const GRPC_SERVER_PORT: &str = "GRPC_SERVER_PORT";
 
 pub fn load_grpc_server_config() -> Result<GRPCServerConfig, String> {
-    let mut config = GRPCServerConfig{ host: "".to_owned(), port: "".to_owned()};
+    let mut config = GRPCServerConfig {
+        host: "".to_owned(),
+        port: "".to_owned(),
+    };
 
     let host = env::var(GRPC_SERVER_HOST);
     if host.is_err() {
-        return Err(format!("missing environment variable: {}", GRPC_SERVER_HOST));
+        return Err(format!(
+            "missing environment variable: {}",
+            GRPC_SERVER_HOST
+        ));
     }
 
     config.host = host.unwrap();
 
     let port = env::var(GRPC_SERVER_PORT);
     if port.is_err() {
-        return Err(format!("missing environment variable: {}", GRPC_SERVER_PORT));
+        return Err(format!(
+            "missing environment variable: {}",
+            GRPC_SERVER_PORT
+        ));
     }
 
     config.port = port.unwrap();
