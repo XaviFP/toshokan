e3019cf3fa46f8b660bd3084eb48879db6338a51
Xavi Ferrer <xavi@delape.net>
2023-03-30 20:03:21 +0200

    shorten test asserts

--- dealer/src/argumenter.rs
+++ dealer/src/argumenter.rs
@@ -21,25 +21,14 @@ fn number_and_order_of_positional_args_is_correct() {
     };
     let dollar_1 = arger.add("first".to_owned());
     let dollar_2 = arger.add("second".to_owned());
-    let dollar_3 = arger.add("thirst".to_owned());
+    let dollar_3 = arger.add("third".to_owned());
 
-    assert_eq!(dollar_1, "$1");
-    assert_eq!(dollar_2, "$2");
-    assert_eq!(dollar_3, "$3");
-}
-
-#[test]
-fn number_and_order_of_args_is_correct() {
-    let mut arger = Argumenter {
-        values: Vec::<String>::new(),
-    };
-    arger.add("first".to_owned());
-    arger.add("second".to_owned());
-    arger.add("thirst".to_owned());
+    assert_eq!(vec!["$1", "$2", "$3"], vec![dollar_1, dollar_2, dollar_3]);
 
     let values = arger.values();
-    assert_eq!(values.len(), 3);
-    assert_eq!(values[2], "thirst");
-    assert_eq!(values[1], "second");
-    assert_eq!(values[0], "first");
+    assert_eq!(3, values.len());
+    assert_eq!(
+        &vec!["first".to_owned(), "second".to_owned(), "third".to_owned()],
+        values
+    );
 }
